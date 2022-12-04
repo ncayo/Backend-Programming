88bff7a97d99cce6f136f9bda789226e5034f00f
yo <92345964+ncayo@users.noreply.github.com>
2022-12-05 00:30:14 +0700

    Update app.js

--- pertemuan9/task/app.js
+++ pertemuan9/task/app.js
@@ -6,22 +6,23 @@
  * @hint - Gunakan Destructing Object
  */
 
- var importHere = "";
-
- /**
-  * NOTES:
-  * - Fungsi main tidak perlu diubah
-  * - Jalankan program: nodejs app.js
-  */
- const main = () => {
-   console.log("Method index - Menampilkan Buah");
-   index();
-   console.log("\nMethod store - Menambahkan buah Pisang");
-   store("Pisang");
-   console.log("\nMethod update - Update data 0 menjadi Kelapa");
-   update(0, "Kelapa");
-   console.log("\nMethod destroy - Menghapus data 0");
-   destroy(0);
- };
+ const {index, store, update, destroy} = 
+ require("./Controllers/FruitController.js");
  
- main();
+  /**
+   * NOTES:
+   * - Fungsi main tidak perlu diubah
+   * - Jalankan program: nodejs app.js
+   */
+  const main = () => {
+    console.log("Method index - Menampilkan Buah");
+    index();
+    console.log("");
+    store("Pisang");
+    console.log("");
+    update(0, "Kelapa");
+    console.log("");
+    destroy(0);
+  };
+  
+  main();
